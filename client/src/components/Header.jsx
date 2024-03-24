261938e1a657b75ead29db05c5ecd095934173f5
Isurinda-Shamika <isurinda25shamika@gmail.com>
2024-03-24 09:29:18 +0530

    update Header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,11 @@
 import { Button, Navbar, TextInput } from 'flowbite-react';
-import { Form, Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import {FaMoon} from 'react-icons/fa';
 
 
 
 export default function Header() {
+    const path = useLocation().pathname;
   return (
     <Navbar className='border-b-2'>
       <Link
@@ -15,9 +17,70 @@ export default function Header() {
         </span>
         SWIFT
       </Link>
+             
+          <div className="flex gap-2  md:order-2">
+            <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
+              <FaMoon/>
+            </Button>
 
-    
-      
+            <Link to='/Sign-In'>
+            <Button  gradientDuoTone='purpleToBlue' >
+              Sign In
+              </Button>
+            </Link>
+           
+          </div>
+
+          <Navbar.Collapse>
+              <Navbar.Link active={path === "/"} as={'div'}>
+                <Link to='/'>
+                  Home
+                </Link>
+              </Navbar.Link>
+            </Navbar.Collapse>
+
+            <Navbar.Collapse>
+              <Navbar.Link active={path === "/News"} as={'div'}>
+                <Link to='/News'>
+                  News
+                </Link>
+              </Navbar.Link>
+            </Navbar.Collapse>
+
+            <Navbar.Collapse>
+              <Navbar.Link active={path === "/Shop"} as={'div'}>
+                <Link to='/Shop'>
+                  Shop
+                </Link>
+              </Navbar.Link>
+            </Navbar.Collapse>
+
+            <Navbar.Collapse>
+              <Navbar.Link active={path === "/Events"} as={'div'}>
+                <Link to='/Events'>
+                  Events
+                </Link>
+              </Navbar.Link>
+            </Navbar.Collapse>
+
+            <Navbar.Collapse>
+              <Navbar.Link active={path === "/AboutUs"} as={'div'}>
+                <Link to='/AboutUs'>
+                  About Us
+                </Link>
+              </Navbar.Link>
+            </Navbar.Collapse>
+
+            <Navbar.Collapse>
+              <Navbar.Link active={path === "/ContactUs"} as={'div'}>
+                <Link to='/ContactUs'>
+                  Contact Us
+                </Link>
+              </Navbar.Link>
+            </Navbar.Collapse>
+
+
+
       </Navbar>
   )
 }
