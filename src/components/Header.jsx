0f607af254293bc3e87ed6b4c65ae1e9477c880f
chey99 <101052229+it21005008@users.noreply.github.com>
2024-03-25 13:20:12 +0530

    logo to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,21 @@
+import { Navbar } from 'flowbite-react'
 import React from 'react'
+import logo from '../assets/images/logo2.png'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <Navbar className=' pt-5'>
+       
+        <Link to="/" className='self-center whitespace-nowrap '>
+        <img src={logo} width={100} height={100}></img>
+        </Link>
+       
+       
+        <ul>
+            <li>g</li>
+        </ul>
+    
+    </Navbar>
   )
 }
