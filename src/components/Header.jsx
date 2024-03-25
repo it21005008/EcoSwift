5dc1d4f96cb83a3ed7affddb29edc3a367eb5e3e
chey99 <101052229+it21005008@users.noreply.github.com>
2024-03-25 15:22:57 +0530

    nav bar moon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,24 +2,32 @@ import { Navbar } from 'flowbite-react';
 import React from 'react';
 import logo from '../assets/images/logo2.png';
 import { Link } from 'react-router-dom';
+import {FaMoon} from 'react-icons/fa';
 
 
 export default function Header() {
   return (
+       <div className='divide-y'>
     <Navbar className=' pt-5'>
-       
-        <Link to="/" className='self-center whitespace-nowrap '>
+        <Link to="/" className='self-center whitespace-nowrap'>
         <img src={logo} width={100} height={100}></img>
         </Link>
        
-       <form>
-            
-       </form>
+  <div></div>
        
-        <ul>
-            <li>g</li>
-        </ul>
+       <div className=''>
+        <button className='w-12 h-10 hidden sm:inline' color='gray' pill>
+            <FaMoon/>       
+            </button>
+        </div>
+       
+        
+
     
     </Navbar>
+        <div class="grid grid-cols-1 divide-y">
+  ffffff
+</div>
+    </div>
   )
 }
