ea6032e8a39771f5fafe15e3c15670f3516c1e80
Supun <IT21271878@my.sliit.lk>
2024-03-27 13:58:08 +0530

    nav bar fix

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
+
 const Navbar = () => {
+
+    const[menu,setMenu]=useState("shop");
+
+
+
+
+
   return (
-    <div ClassName='navbar'>
-    </div>
+    <div className="navbar">
+        
+        
+        <div className="nav-logo">
+    <img src={logo} alt="" style={{ width: '160px', height: '60px' }} />
+    
+</div>
+
+           <ul className="nav-menu">
+          <li onClick={()=>{setMenu("Home")}}>Home{menu=="Home"?<h/>:<></>}</li>
+           <li onClick={()=>{setMenu("news")}}>news{menu=="news"?<h/>:<></>}</li>
+           <li onClick={()=>{setMenu("Shop")}}>shop{menu=="shop"?<h/>:<></>}</li>
+           <li onClick={()=>{setMenu("events")}}>events{menu=="events"?<h/>:<></>}</li>
+           </ul>
+
+           <div className= "nav-login-cart">
+            <button>Login</button>
+            <img src={cart_icon} alt=""/>
+            <div className="nav-cart-count">0</div>
+
+            
+           </div>
+
+           </div>
+
+
+
+
+    
   )
 }
 
