cdb483533234f9dca07e04aabcd6f777b18061cc
Anishka <it21294716@my.sliit.lk>
2024-03-28 01:18:54 +0530

    mongodb_resolved

--- server/index.js
+++ server/index.js
@@ -8,10 +8,17 @@ app.use(express.json())
 app.use(cors())
 
 
-mongoose.connect("mongodb+srv://ECOSWIFT:ecoswift@ecoswift.8hegwtb.mongodb.net/?retryWrites=true&w=majority&appName=ECOSWIFT");
-
-
-
-app.listen(3000, () => {
-    console.log("server is running on port 3000")
+// MongoDB connection URI
+const mongoURI = 'mongodb+srv://ECOSWIFT:ECOSWIFT@ecoswift.8hegwtb.mongodb.net/?retryWrites=true&w=majority&appName=ECOSWIFT';
+// Connect to MongoDB
+mongoose.connect(mongoURI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+        // Start your application logic here
+    })
+    .catch((err) => {
+        console.error('Failed to connect to MongoDB:', err);
+    });
+app.listen(3550, () => {
+    console.log("server is running on port 4000")
 });
