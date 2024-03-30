d7a396188e03bbefa21e6e9ceb21732fcff7cf85
chey99 <101052229+it21005008@users.noreply.github.com>
2024-03-30 08:54:23 +0530

    successfully connected data base

--- api/index.js
+++ api/index.js
@@ -1,8 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb+srv://ECOSWIFT:ECOSWIFT@ecoswift.8hegwtb.mongodb.net/ECOSWIFT?retryWrites=true&w=majority&appName=ECOSWIFT')
+    .then(() => {
+        console.log('Connected to MongoDB');
+    });
 
 const app = express();
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
-    }
-);
+});
+ 
